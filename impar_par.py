11db751635f7620d11d2b58a9219ccc344cbacea
Leonardo Flores <scrowws@gmail.com>
2025-03-22 15:25:46 -0300

    Add files via upload
    Atividade para descobrir se um numero e impar ou par

---
+++ impar_par.py
@@ -0,0 +1,6 @@
+numero = int(input("Informe um numero para descobrir se ele e par ou impar:"))
+resultado = numero%2
+if resultado == 0:
+    print("O numero e par")
+else:
+    print("O numero e impar")

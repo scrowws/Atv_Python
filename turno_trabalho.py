af137745b6dc0d47c1126f87fcc5e2b974d99669
Leonardo Flores <scrowws@gmail.com>
2025-03-22 15:52:40 -0300

    Add files via upload

---
+++ turno_trabalho.py
@@ -0,0 +1,10 @@
+print("M = Manha | T = Tarde | N = Noite")
+turno = input(print("Informe o turno em que trabalha:"))
+if turno == "M":
+    print("Bom dia!")
+elif turno == "T":
+    print("Boa tarde!")
+elif turno == "N":
+    print("Boa noite!")
+else:
+    print("Entrada invalida")

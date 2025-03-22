98eb79d986cfd8baa7f19dfb57f6a985c0b5c182
Leonardo Flores <scrowws@gmail.com>
2025-03-22 15:17:10 -0300

    Add files via upload

---
+++ Nome_cartao.py
@@ -0,0 +1,6 @@
+nome = input("Informe o seu nome:")
+caracteres = len(nome)
+if caracteres > 20:
+    print("Nome é extenso demais e deve ser abreviado!")
+else:
+    print("Cadastrado com sucesso")

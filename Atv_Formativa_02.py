92fc7845ac6f905cdb0ca2b6906096876b7e5a79
Leonardo Flores <scrowws@gmail.com>
2025-03-22 15:10:08 -0300

    Add files via upload
    Atividade apenas para pratica de uso de print e input.

---
+++ Atv_Formativa_02.py
@@ -0,0 +1,6 @@
+print("Verificacao de idade")
+idade = int(input("Por favor, informe sua idade"))
+if idade >= 18:
+    print("Voce ja pode se cadastrar para o teste para emissao de CNH")
+else:
+    print("Voce ainda e menor de idade")
